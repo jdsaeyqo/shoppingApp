12601483de046e55756fb9416bc08034f638eb66
Sang <jdsaeyqi@naver.com>
2021-06-15 15:12:45 +0900

    DI 추가

--- app/src/main/java/com/example/shoppingapp/di/AppModule.kt
+++ app/src/main/java/com/example/shoppingapp/di/AppModule.kt
@@ -4,9 +4,23 @@ import com.example.shoppingapp.data.network.buildOkHttpClient
 import com.example.shoppingapp.data.network.provideGsonConverterFactory
 import com.example.shoppingapp.data.network.provideProductApiService
 import com.example.shoppingapp.data.network.provideProductRetrofit
+import com.example.shoppingapp.data.repository.DefaultProductRepository
+import com.example.shoppingapp.data.repository.ProductRepository
+import com.example.shoppingapp.domain.GetProductItemUseCase
+import kotlinx.coroutines.Dispatchers
 import org.koin.dsl.module
 
 val appModule = module {
+
+    // Coroutines Dispatcher
+    single { Dispatchers.Main }
+    single { Dispatchers.IO }
+
+    //Repositories
+    single<ProductRepository> { DefaultProductRepository(get(),get()) }
+
+    //UseCases
+    factory { GetProductItemUseCase(get()) }
 
     single { provideGsonConverterFactory() }
 
