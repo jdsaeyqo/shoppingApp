fc7a4e4f9c60a69e92ed571f9805b0b3e4e5ebc1
Sang <jdsaeyqi@naver.com>
2021-06-15 14:39:29 +0900

    Response

---
+++ app/src/main/java/com/example/shoppingapp/data/response/ProductResponse.kt
@@ -0,0 +1,27 @@
+package com.example.shoppingapp.data.response
+
+import com.example.shoppingapp.data.entity.product.ProductEntity
+import java.util.*
+
+data class ProductResponse(
+
+    val id : Long,
+    val createdAt: Long,
+    val productName: String,
+    val productPrice: String,
+    val productImage: String,
+    val productType: String,
+    val productIntroductionImage: String
+
+){
+    fun toEntity() : ProductEntity =
+        ProductEntity(
+            id = id.toLong(),
+            createdAt = Date(createdAt),
+            productName = productName,
+            productPrice = productPrice.toDouble().toInt(),
+            productImage = productImage,
+            productType = productType,
+            productIntroductionImage = productIntroductionImage
+        )
+}
---
+++ app/src/main/java/com/example/shoppingapp/data/response/ProductsResponse.kt
@@ -0,0 +1,6 @@
+package com.example.shoppingapp.data.response
+
+data class ProductsResponse(
+    val item : List<ProductResponse>,
+    val count : Int
+)
