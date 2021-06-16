8503bf74c65fbea068950013fc3f899888bb4603
Sang <jdsaeyqi@naver.com>
2021-06-16 20:09:53 +0900

    의존성 주입 추가

--- app/src/main/java/com/example/shoppingapp/di/AppModule.kt
+++ app/src/main/java/com/example/shoppingapp/di/AppModule.kt
@@ -7,17 +7,26 @@ import com.example.shoppingapp.data.network.provideProductRetrofit
 import com.example.shoppingapp.data.repository.DefaultProductRepository
 import com.example.shoppingapp.data.repository.ProductRepository
 import com.example.shoppingapp.domain.GetProductItemUseCase
+import com.example.shoppingapp.presentation.main.MainViewModel
+import com.example.shoppingapp.presentation.profile.ProfileViewModel
+import com.example.shoppingapp.presentation.shoppinglist.ProductListViewModel
 import kotlinx.coroutines.Dispatchers
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val appModule = module {
+internal val appModule = module {
+
 
     // Coroutines Dispatcher
-    single { Dispatchers.Main }
     single { Dispatchers.IO }
 
+    //ViewModel
+    viewModel{ MainViewModel() }
+    viewModel { ProductListViewModel() }
+    viewModel { ProfileViewModel() }
+
     //Repositories
-    single<ProductRepository> { DefaultProductRepository(get(),get()) }
+    single<ProductRepository> { DefaultProductRepository(get(), get()) }
 
     //UseCases
     factory { GetProductItemUseCase(get()) }
