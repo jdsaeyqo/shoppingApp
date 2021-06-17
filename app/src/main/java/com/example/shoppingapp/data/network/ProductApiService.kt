e9325cd823a1291c37fc911305f4798d52d256dc
Sang <jdsaeyqi@naver.com>
2021-06-17 16:53:39 +0900

    오타 수정

--- app/src/main/java/com/example/shoppingapp/data/network/ProductApiService.kt
+++ app/src/main/java/com/example/shoppingapp/data/network/ProductApiService.kt
@@ -1,6 +1,7 @@
 package com.example.shoppingapp.data.network
 
 import com.example.shoppingapp.data.response.ProductResponse
+import com.example.shoppingapp.data.response.ProductsResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -8,7 +9,7 @@ import retrofit2.http.Path
 interface ProductApiService {
 
     @GET("products")
-    suspend fun getProduct() : Response<ProductResponse>
+    suspend fun getProducts() : Response<ProductsResponse>
 
     @GET("products/{productId}")
     suspend fun getProduct(@Path("productId") production : Long) : Response<ProductResponse>
--- app/src/main/java/com/example/shoppingapp/data/network/Url.kt
+++ app/src/main/java/com/example/shoppingapp/data/network/Url.kt
@@ -1,6 +1,6 @@
 package com.example.shoppingapp.data.network
 
-object Url {
+object  Url {
 
     const val PRODUCT_BASE_URL = "https://6083a15d5dbd2c001757b94a.mockapi.io/part5/chapter02/"
 
